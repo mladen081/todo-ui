9732c6dc572c1f1015a0b3cdb6061fd72fee3931
mladen081 <jovicmladen094@gmail.com>
2024-07-14 13:35:23 +0200

    002 start css 1

--- src/components/ListTodoComponent.jsx
+++ src/components/ListTodoComponent.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getAllTodos } from "../services/TodoService";
-
+import "./ListTodoComponent.css";
 const ListTodoComponent = () => {
   const [todos, setTodos] = useState([]);
 
@@ -21,14 +21,22 @@ const ListTodoComponent = () => {
   return (
     <div className="container">
       <h1>List of Todos</h1>
+      <br />
+      <br />
       {todos.map((todo) => (
         <div key={todo.id}>
           <h2>Todo Title</h2>
           <p>{todo.title}</p>
+          <br />
+          <br />
           <h2>Todo Description</h2>
           <p>{todo.description}</p>
+          <br />
+          <br />
           <h2>Todo Completed</h2>
           <p>{todo.completed ? "yes" : "no"}</p>
+          <br />
+          <br />
           <hr />
         </div>
       ))}
