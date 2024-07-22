99d666281fa295d6c0bf9d2ecec0558d4206aecf
mladen081 <jovicmladen094@gmail.com>
2024-07-22 20:57:21 +0200

    016 reverted to 013, with fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
+import { useState } from "react";
 import "./App.css";
-import FooterComponent from "./components/FooterComponent";
-import HeaderComponent from "./components/HeaderComponent";
 import ListTodoComponent from "./components/ListTodoComponent";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import TodoComponent from "./components/TodoComponent";
 import RegisterComponent from "./components/RegisterComponent";
 import LoginComponent from "./components/LoginComponent";
+import { isUserLoggedIn } from "./services/AuthService";
 
 function App() {
+  function AuthenticatedRoute({ children }) {
+    const isAuth = isUserLoggedIn();
+
+    if (isAuth) {
+      return children;
+    }
+
+    return <Navigate to="/" />;
+  }
+
   return (
     <>
       <BrowserRouter>
+        <HeaderComponent />
         <Routes>
+          {/* http://localhost:8080 */}
           <Route path="/" element={<LoginComponent />}></Route>
-          <Route path="/todos" element={<ListTodoComponent />}></Route>
-          <Route path="/add-todo" element={<TodoComponent />}></Route>
-          <Route path="/update-todo/:id" element={<TodoComponent />}></Route>
-
+          {/* http://localhost:8080/todos */}
+          <Route
+            path="/todos"
+            element={
+              <AuthenticatedRoute>
+                <ListTodoComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
+          {/* http://localhost:8080/add-todo */}
+          <Route
+            path="/add-todo"
+            element={
+              <AuthenticatedRoute>
+                <TodoComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
+          {/* http://localhost:8080/update-todo/1 */}
+          <Route
+            path="/update-todo/:id"
+            element={
+              <AuthenticatedRoute>
+                <TodoComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
+          {/* http://localhost:8080/register */}
           <Route path="/register" element={<RegisterComponent />}></Route>
+
+          {/* http://localhost:8080/login */}
           <Route path="/login" element={<LoginComponent />}></Route>
         </Routes>
-        <HeaderComponent />
         <FooterComponent />
       </BrowserRouter>
     </>
